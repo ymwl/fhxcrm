578f9a8c1ef884480567e829c641ba0debd775e7
ymwl <zx80com@163.com>
2026-06-13 23:21:25 +0800

    feat(crm): 新增客户领取限制服务并清理废弃文件
    - 新增客户领取次数统计与限制的业务服务类
    - 新增CRM客户模块中文语言包
    - 删除废弃的分页配置、模型视图文件及uniapp签到页面

---
+++ app/service/CrmClueService.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * 客户相关静态方法
+ * 
+ */
+
+namespace app\service;
+
+use think\facade\Db;
+
+class CrmCustomerService
+{
+    /**
+     * 获取客户领取次数信息（静态方法）
+     * @param array $system 系统配置
+     * @param int $adminId 管理员ID
+     * @return array
+     */
+    public static function getGrabCount($system, $adminId)
+    {
+        $cycleList = \app\admin\model\System::getCycleList();
+        if (isset($system['customer_limit_counts']) && $system['customer_limit_counts'] > 0 && isset($cycleList[$system['customer_limit_condition']])) {
+            if ($system['customer_limit_condition'] == 'day') {
+                $starttime = mktime(0, 0, 0, (int)date('n'), (int)date('j'), (int)date('Y'));
+            } elseif ($system['customer_limit_condition'] == 'week') {
+                $starttime = mktime(0, 0, 0, (int)date('n'), date('d') - date('w') + 1, (int)date('Y'));
+            } else {
+                $starttime = mktime(0, 0, 0, (int)date('m'), 1, (int)date('Y'));
+            }
+
+            $crmGrabCount = Db::name('crm_grab')->where("`createtime` >= {$starttime} and `admin_id`={$adminId}")->sum('nums');
+            if ($crmGrabCount >= $system['customer_limit_counts']) {
+                return ['code' => false, 'msg' => fy('The number of times customers can be claimed has been used up')];
+            } else {
+                $shy = $system['customer_limit_counts'] - $crmGrabCount;
+                return ['code' => true, 'msg' => $cycleList[$system['customer_limit_condition']] . ' ' . fy('remaining number of times can be claimed') . '：' . $shy, 'count' => $shy];
+            }
+        } else {
+            return ['code' => true, 'msg' => ''];
+        }
+    }
+}
--- config/paginate.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'type'      => 'clt\Bootstrap',
-    'var_page'  => 'page',
-    'list_rows' => 8,
-];
